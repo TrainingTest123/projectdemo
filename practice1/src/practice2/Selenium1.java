52f4c76be26beb34621a0586cd3c2a1c9b055f2a
shravani <shravani@gmail.com>
2020-01-01 11:39:55 -0500

    Added new line

--- practice1/src/practice2/Selenium1.java
+++ practice1/src/practice2/Selenium1.java
@@ -2,8 +2,10 @@ package practice2;
 
 public class Selenium1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// frtTODO Auto-generated method stub
+		System.out.println("Github configured");
 
 	}
 
